afc1164817c8c08205ca651abbb56601d8e1d53d
DY <dave1315@korea.ac.kr>
2023-07-25 01:45:23 +0900

    발소리
    캐릭터 애니메이션도

---
+++ Source/KillLight/Character/MainAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainAnimInstance.h"
+#include "MainCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UMainAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	MainCharacter = Cast<AMainCharacter>(TryGetPawnOwner());
+}
+
+void UMainAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (MainCharacter == nullptr)
+	{
+		MainCharacter = Cast<AMainCharacter>(TryGetPawnOwner());
+	}
+	if (MainCharacter == nullptr) return;
+
+	FVector Velocity = MainCharacter->GetVelocity();
+	Velocity.Z = 0.0f;
+	Speed = Velocity.Size();
+
+	FRotator AimRotation = MainCharacter->GetBaseAimRotation();
+	FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(MainCharacter->GetVelocity());
+	YawOffset = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
+}
---
+++ Source/KillLight/Character/MainAnimInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KILLLIGHT_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	class AMainCharacter* MainCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float YawOffset;
+
+};
